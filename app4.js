afc23ed278a3aab448dd0a1c9413babf36709011
Autumn R. Mosley <36770891+hatbearingoctopus@users.noreply.github.com>
2019-07-02 21:06:55 -0700

    Update app4.js

--- app4.js
+++ app4.js
@@ -20,13 +20,25 @@ const todos = [{
 }]
 
 const filters = {
-  searchText: ''
+  searchText: '',
+  hideCompleted: false
 }
 
 //figures out which todo matches the filter
 const renderTodos = function (todos, filters) {
-  const filteredTodos = todos.filter(function (todo) {
+  let filteredTodos = todos.filter(function (todo) {
     return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
+  
+  filteredTodos = filteredTodos.filter(function (todo) {
+    //option one
+    return !filters.hideCompleted || !todo.completed
+    //option two
+    // if (filters.hideCompleted) {
+    //     return !todo.completed 
+    // } else {
+    //    return true
+    // }
   })
 
   const incompleteTodos = filteredTodos.filter(function (todo) {
@@ -48,19 +60,26 @@ filteredTodos.forEach(function (todo) {
 
 renderTodos(todos, filters)
 
-//listen for new todo creation
-document.querySelector('#add-todo-btn').addEventListener('click', function (e) {
-  console.log('learning about those click events yo!')
-})
-
-//listen for todo text change
-document.querySelector('#add-new-todo').addEventListener('input', function (e) {
-  console.log(e.target.value)
-})
-
 //updates filter object
 document.querySelector('#search-text').addEventListener('input', function (e) {
   filters.searchText = e.target.value
+  renderTodos(todos, filters)
+})
+
+//event listener for the submit, pushes entered todo to todos array, prevents default, re-renders todos with current filters and removes previously entered item on submit
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+  e.preventDefault()
+  todos.push({
+    text: e.target.elements.text.value,
+    completed: false
+  })
+  renderTodos(todos, filters)
+  e.target.elements.text.value = ''
+})
+
+//event listener to hide completed todos when checkbox is clicked
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+  filters.hideCompleted = e.target.checked
   renderTodos(todos, filters)
 })
 
