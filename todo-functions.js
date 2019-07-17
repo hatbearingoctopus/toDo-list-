adb3dd17e03ad588381abe3cca898bba55846d01
Autumn R. Mosley <hatbearingoctopus@gmail.com>
2019-07-17 13:43:18 -0700

    Update todo-functions.js

--- todo-functions.js
+++ todo-functions.js
@@ -25,6 +25,17 @@ const removeTodo = function (id) {
   }
 }
 
+// Toggle the completed value for a given todo
+const toggleTodo = function (id) {
+  const todo = todos.find(function (todo) {
+      return todo.id === id
+  })
+
+  if (todo !== undefined) {
+      todo.completed = !todo.completed
+  }
+}
+
 // Render application todos based on filters
 const renderTodos = function (todos, filters) {
   const filteredTodos = todos.filter(function (todo) {
@@ -55,11 +66,37 @@ const generateTodoDOM = function (todo) {
 
   // Setup todo checkbox
   checkbox.setAttribute('type', 'checkbox')
+  // Checkbox is checked with todo is completed
+  checkbox.checked = todo.completed
   todoEl.appendChild(checkbox)
+  checkbox.addEventListener('change', function () {
+      toggleTodo(todo.id)
+      saveTodos(todos)
+      renderTodos(todos, filters)
+  })
 
   // Setup the todo text
   todoText.textContent = todo.text
   todoEl.appendChild(todoText)
+
+  // Setup the remove button
+  removeButton.textContent = 'x'
+  todoEl.appendChild(removeButton)
+  removeButton.addEventListener('click', function () {
+      removeTodo(todo.id)
+      saveTodos(todos)
+      renderTodos(todos, filters)
+  })
+
+  return todoEl
+}
+
+// Get the DOM elements for list summary
+const generateSummaryDOM = function (incompleteTodos) {
+  const summary = document.createElement('h2')
+  summary.textContent = `You have ${incompleteTodos.length} todos left`
+  return summary
+}
 
   // Setup the remove button
   removeButton.textContent = 'x'
